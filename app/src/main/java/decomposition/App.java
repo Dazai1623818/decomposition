572fcc32b29779717cb17dcd798a779390e3212f
Calvin <c.c.e.lee@student.tue.nl>
2025-07-12 19:42:15 +0200

    moved component initialization down

--- app/src/main/java/decomposition/App.java
+++ app/src/main/java/decomposition/App.java
@@ -1,16 +1,29 @@
 package decomposition;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import decomposition.Partitioning.Edge;
 import decomposition.QueryUtils.ComponentInfo;
-import dev.roanh.gmark.lang.cpq.*;
-import dev.roanh.gmark.lang.cq.*;
-import dev.roanh.gmark.type.schema.Predicate;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static decomposition.QueryUtils.*;
+import static decomposition.QueryUtils.buildCQFromEdges;
+import static decomposition.QueryUtils.constructCPQFromEdges;
+import static decomposition.QueryUtils.extractFreeVariableNames;
+import static decomposition.QueryUtils.getVarsFromEdges;
+import static decomposition.QueryUtils.isEquivalent;
+import static decomposition.QueryUtils.printCQAtoms;
 import static decomposition.Util.exportAllPartitionsToJson;
+import static decomposition.Util.exportFreeVarsToJson;
+import dev.roanh.gmark.lang.cpq.CPQ;
+import dev.roanh.gmark.lang.cpq.ConcatCPQ;
+import dev.roanh.gmark.lang.cpq.EdgeCPQ;
+import dev.roanh.gmark.lang.cpq.IntersectionCPQ;
+import dev.roanh.gmark.lang.cq.CQ;
+import dev.roanh.gmark.lang.cq.VarCQ;
+import dev.roanh.gmark.type.schema.Predicate;
 
 public class App {
 
@@ -30,7 +43,7 @@ public class App {
         cq.addAtom(b, new Predicate(2, "r2"), c);
         cq.addAtom(c, new Predicate(3, "r3"), d);
         cq.addAtom(d, new Predicate(4, "r4"), a);
-        // cq.addAtom(a, new Predicate(5, "r5"), d);
+        cq.addAtom(a, new Predicate(5, "r5"), c);
 
         return cq;
     }
@@ -72,29 +85,12 @@ public class App {
         return filtered;
     }
 
-    private static Map<List<Edge>, ComponentInfo> initializeKnownComponents(List<List<List<Edge>>> filteredPartitions) {
-        Map<List<Edge>, ComponentInfo> known = new HashMap<>();
-        if (filteredPartitions.isEmpty()) return known;
 
-        List<List<Edge>> firstPartition = filteredPartitions.get(0);
-        for (List<Edge> component : firstPartition) {
-            Predicate p = component.get(0).getPredicate();
-            CPQ cpq = new EdgeCPQ(p);
-            CQ subCq = buildCQFromEdges(component);
-            Set<String> vars = getVarsFromEdges(component);
-            known.put(component, new ComponentInfo(subCq, cpq, vars, true));
-
-            System.out.println("Initial known CPQ: " + p.getAlias());
-        }
-        return known;
-    }
 
     public static void processPartition(List<List<Edge>> partition, Map<List<Edge>, ComponentInfo> componentMap) {
         for (List<Edge> component : partition) {
             ComponentInfo info = componentMap.get(component);
             if (info != null && info.isKnown) {
-                System.out.println("Component already known:");
-                component.forEach(e -> System.out.println("  " + e));
                 continue;
             }
 
@@ -112,9 +108,8 @@ public class App {
     }
 
     public static void tryMatchWithKnownComponents(List<Edge> component,
-                                                   Map<List<Edge>, ComponentInfo> componentMap) {
+                                                Map<List<Edge>, ComponentInfo> componentMap) {
         Set<Edge> targetEdges = new HashSet<>(component);
-
         List<List<Edge>> knownComponents = componentMap.entrySet().stream()
                 .filter(e -> e.getValue().isKnown)
                 .map(Map.Entry::getKey)
@@ -157,10 +152,33 @@ public class App {
 
                     if (isEquivalent(intersection, target)) {
                         System.out.println("Match found by conjunction (intersection of cpq1 and cpq2)");
-                    componentMap.put(component, componentMap.get(component).markKnown());
-                    return;
+                        componentMap.put(component, componentMap.get(component).markKnown());
+                        return;
+                    }
                 }
             }
         }
+    }
+
+
+
+
+
+
+    private static Map<List<Edge>, ComponentInfo> initializeKnownComponents(List<List<List<Edge>>> filteredPartitions) {
+        Map<List<Edge>, ComponentInfo> known = new HashMap<>();
+        if (filteredPartitions.isEmpty()) return known;
+
+        List<List<Edge>> firstPartition = filteredPartitions.get(0);
+        for (List<Edge> component : firstPartition) {
+            Predicate p = component.get(0).getPredicate();
+            CPQ cpq = new EdgeCPQ(p);
+            CQ subCq = buildCQFromEdges(component);
+            Set<String> vars = getVarsFromEdges(component);
+            known.put(component, new ComponentInfo(subCq, cpq, vars, true));
+
+            System.out.println("Initial known CPQ: " + p.getAlias());
+        }
+        return known;
     }
 }
