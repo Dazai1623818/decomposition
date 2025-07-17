c5ce7383b30f31e147a863057ab16cb60ea5d2b8
Dazai <c.c.e.lee@student.tue.nl>
2025-07-17 23:55:46 +0200

    seperate example file

---
+++ app/src/main/java/decomposition/Example.java
@@ -0,0 +1,57 @@
+package decomposition;
+
+import dev.roanh.gmark.lang.cq.CQ;
+import dev.roanh.gmark.lang.cq.VarCQ;
+import dev.roanh.gmark.type.schema.Predicate;
+
+public class Example {
+
+    public static CQ example1() {
+        CQ cq = CQ.empty();
+        VarCQ a = cq.addFreeVariable("A");
+        VarCQ b = cq.addBoundVariable("B");
+        VarCQ c = cq.addBoundVariable("C");
+        VarCQ d = cq.addBoundVariable("D");
+
+        cq.addAtom(a, new Predicate(1, "r1"), b);
+        cq.addAtom(b, new Predicate(2, "r2"), c);
+        cq.addAtom(c, new Predicate(3, "r3"), d);
+        cq.addAtom(d, new Predicate(4, "r4"), a);
+        cq.addAtom(a, new Predicate(5, "r5"), c);
+
+        return cq;
+    }
+
+    public static CQ example2() {
+        CQ cq = CQ.empty();
+        VarCQ b = cq.addFreeVariable("B");
+        VarCQ a = cq.addBoundVariable("A");
+        VarCQ c = cq.addBoundVariable("C");
+        VarCQ d = cq.addBoundVariable("D");
+
+        cq.addAtom(a, new Predicate(1, "r1"), b);
+        cq.addAtom(b, new Predicate(2, "r2"), c);
+        cq.addAtom(c, new Predicate(3, "r3"), d);
+        cq.addAtom(d, new Predicate(4, "r4"), a);
+        cq.addAtom(a, new Predicate(5, "r5"), c);
+
+        return cq;
+    }
+
+    public static CQ example3() {
+        CQ cq = CQ.empty();
+        VarCQ a = cq.addFreeVariable("A");
+        VarCQ b = cq.addBoundVariable("B");
+        VarCQ c = cq.addBoundVariable("C");
+        VarCQ d = cq.addBoundVariable("D");
+
+        cq.addAtom(a, new Predicate(1, "r1"), b);
+        cq.addAtom(b, new Predicate(2, "r2"), c);
+        cq.addAtom(c, new Predicate(3, "r3"), d);
+        cq.addAtom(d, new Predicate(4, "r4"), a);
+        cq.addAtom(a, new Predicate(5, "r5"), c);
+        cq.addAtom(a, new Predicate(6, "r1"), a);
+
+        return cq;
+    }
+}
